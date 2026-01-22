753837a2cbf6e199a26e795e196a8e11a6df9b47
Fernando <barbaglianeto@gmail.com>
2026-01-22 19:35:50 -0300

    2 hrs para achar um erro(;)

--- arquivo002/main.java
+++ arquivo002/main.java
@@ -1,5 +1,7 @@
 package arquivo002;
 
 public class main {
-    
+    public static void main(String[] args) {
+        System.out.println("ola mundo");
+    }
 }
---
+++ arquivo003/main.java
@@ -0,0 +1,12 @@
+package arquivo003;
+
+import java.util.Date;
+
+public class main {
+    public static void main(String[] args) {
+        Date relogio = new Date();    
+
+        System.out.println("ola mundo");
+        System.out.println("agora sao " + relogio + " horas");
+    }
+}
